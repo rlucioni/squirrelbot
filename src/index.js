f57116f1426a88bc83a0e88b87e7dcdeb0ad2c03
Renzo Lucioni <renzolucioni@gmail.com>
2025-12-07 22:22:56 -0500

    only message slack when not connected

--- src/index.js
+++ src/index.js
@@ -2,20 +2,21 @@ import 'dotenv/config';
 import functions from '@google-cloud/functions-framework';
 import { WebClient } from '@slack/web-api';
 
-functions.http('squirrelbot', async (req, res) => {
+functions.http('squirrelbot', async (_req, res) => {
+  const TARGET_HOST_NAME = 'UCK-G2';
+
+  const UNIFI_API_KEY = process.env.UNIFI_API_KEY;
+  const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN;
+  const SLACK_CHANNEL_ID = process.env.SLACK_CHANNEL_ID;
+
+  if (!UNIFI_API_KEY || !SLACK_BOT_TOKEN || !SLACK_CHANNEL_ID) {
+    console.error('missing environment variables');
+    return res.status(500).send('config error');
+  }
+
+  const slack = new WebClient(SLACK_BOT_TOKEN);
+
   try {
-    const UNIFI_API_KEY = process.env.UNIFI_API_KEY;
-    const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN;
-    const SLACK_CHANNEL_ID = process.env.SLACK_CHANNEL_ID;
-    const TARGET_HOST_NAME = 'UCK-G2';
-
-    if (!UNIFI_API_KEY || !SLACK_BOT_TOKEN || !SLACK_CHANNEL_ID) {
-      console.error('missing environment variables');
-      return res.status(500).send('config error');
-    }
-
-    const slack = new WebClient(SLACK_BOT_TOKEN);
-
     // https://developer.ui.com/site-manager-api/list-hosts
     const unifiResponse = await fetch('https://api.ui.com/v1/hosts', {
       method: 'GET',
@@ -26,9 +27,12 @@ functions.http('squirrelbot', async (req, res) => {
     });
 
     if (!unifiResponse.ok) {
-      throw new Error(
-        `unifi api error: ${unifiResponse.status} ${unifiResponse.statusText}`,
-      );
+      await slack.chat.postMessage({
+        channel: SLACK_CHANNEL_ID,
+        text: `Unifi API error, status ${unifiResponse.status}`,
+      });
+
+      throw new Error(`unifi api error, status ${unifiResponse.status}`);
     }
 
     const data = await unifiResponse.json();
@@ -41,19 +45,34 @@ functions.http('squirrelbot', async (req, res) => {
     if (targetHost) {
       const state = targetHost.reportedState.state;
 
+      console.info(`found host ${TARGET_HOST_NAME}, state: ${state}`);
+
+      if (state.toLowerCase() !== 'connected') {
+        await slack.chat.postMessage({
+          channel: SLACK_CHANNEL_ID,
+          text: `Host *${TARGET_HOST_NAME}* is currently *${state}*`,
+        });
+      }
+
+      res.status(200).send(`found host, state: ${state}`);
+    } else {
+      console.warn(`host ${TARGET_HOST_NAME} not found`);
+
       await slack.chat.postMessage({
         channel: SLACK_CHANNEL_ID,
-        text: `Host *${TARGET_HOST_NAME}* is currently *${state}*`,
+        text: `Host *${TARGET_HOST_NAME}* not found`,
       });
 
-      console.info(`found host ${TARGET_HOST_NAME}, state: ${state}`);
-      res.status(200).send(`check complete, state: ${state}`);
-    } else {
-      console.warn(`host with name "${TARGET_HOST_NAME}" not found`);
-      res.status(404).send('target host not found in account');
+      res.status(404).send('host not found');
     }
   } catch (error) {
     console.error('error executing function:', error.message);
+
+    await slack.chat.postMessage({
+      channel: SLACK_CHANNEL_ID,
+      text: `Error executing function: ${error.message}`,
+    });
+
     res.status(500).send('internal server error');
   }
 });
